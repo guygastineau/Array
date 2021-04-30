f04ce35f6a068fe17d7782fe6a7d32e80f19651f
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-04-30 08:45:34 -0400

    Initialized from cman defaults

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "src/array.h"
+
+#include <stdio.h>
+
+// Simple stub file for binary projects.
+// Hint: you should erase these comments ;)
+
+int main()
+{
+  printf("%s\n", hello());
+}
---
+++ src/array.c
@@ -0,0 +1,6 @@
+#include "array.h"
+
+const char *hello()
+{
+  return "Hello, World!";
+}
