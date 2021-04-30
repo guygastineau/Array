f8165612ce5ee815f89f9ac614bee1ea54590f20
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-04-30 10:54:16 -0400

    Added the first test

--- test/test_array.c
+++ test/test_array.c
@@ -1,5 +1,14 @@
-#include "vendor/unity.h"
+// Change the configurable macros, so we can test they
+// provide the configurability they should.
+
+// The array sizes will only ever be powers of 2.x
+#define ARRAY_MIN_CAP 8
+#define ARRAY_GROW_RATIO 0.80
+#define ARRAY_SHRINK_RATIO 0.3
+
 #include "../src/array.h"
+
+#include "vendor/unity.h"
 
 void setUp(void)
 {
@@ -9,16 +18,23 @@ void tearDown(void)
 {
 }
 
-static void test_lib(void)
+static void test_allocate_empty_array(void)
 {
-  TEST_ASSERT_EQUAL_STRING("Hello, World!", hello());
+  Array *arr = mkArray();
+  TEST_ASSERT_NOT_NULL_MESSAGE(arr, "mkArray failed to allocate");
+  TEST_ASSERT_EQUAL_size_t_MESSAGE(arr->cap, ARRAY_MIN_CAP,
+                                   "mkArray failed to use ARRAY_MIN_CAP");
+  TEST_ASSERT_EQUAL_size_t_MESSAGE(arr->count, 0,
+                                   "mkArray new array size not 0");
+  TEST_ASSERT_NOT_NULL_MESSAGE(arr->data,
+                               "mkArray failed to allocate internal buffer");
 }
 
 int main(void)
 {
    UnityBegin("test/test_array.c");
 
-   RUN_TEST(test_lib);
+   RUN_TEST(test_allocate_empty_array);
 
    return UnityEnd();
 }
