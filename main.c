466d476698e35e425329153c46d94d2e2a4e569b
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-04-30 10:39:54 -0400

    Remove references to hello

--- main.c
+++ main.c
@@ -2,10 +2,7 @@
 
 #include <stdio.h>
 
-// Simple stub file for binary projects.
-// Hint: you should erase these comments ;)
-
 int main()
 {
-  printf("%s\n", hello());
+  puts("Library for arrays.");
 }
