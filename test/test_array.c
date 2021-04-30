aa571d52d637c6c14d02b5c95fe16cf3c0c3296d
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-04-30 14:08:27 -0400

    Add tests for the initialize from CArray allocator

--- test/test_array.c
+++ test/test_array.c
@@ -5,12 +5,25 @@
 
 #include "vendor/unity.h"
 
+#include <stdbool.h>
+
 void setUp(void)
 {
 }
 
 void tearDown(void)
 {
+}
+
+static void TEST_ASSERT_EQUAL_ARRAY(Slice expected, Array *actual)
+{
+  TEST_ASSERT_NOT_NULL_MESSAGE(actual, "NULL pointer in array comparison");
+  TEST_ASSERT_EQUAL_size_t_MESSAGE(expected.count, actual->count,
+                                   "array count doesn't match");
+  for (size_t i = 0; i < expected.count; ++i) {
+    TEST_ASSERT_EQUAL_INT_MESSAGE(expected.data[i], actual->data[i],
+                                  "array mismatched elements");
+  }
 }
 
 // Helper to ensure that arrays initializer functions work properly.
@@ -49,6 +62,35 @@ static void test_allocate_array_with_capacity_larger(void)
   arrayDestroy(arr);
 }
 
+static void test_initialize_from_buffer_empty(void)
+{
+  TEST_IGNORE();
+  Array *arr = arrayFromCArray(0, NULL);
+  assert_clean_allocation(arr, ARRAY_MIN_CAP, 0);
+  arrayDestroy(arr);
+}
+
+static void test_initialize_from_buffer_with_data(void)
+{
+  TEST_IGNORE();
+  data_t data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  Array *arr = arrayFromCArray(10, data);
+  assert_clean_allocation(arr, ARRAY_MIN_CAP, 10);
+  TEST_ASSERT_EQUAL_ARRAY((Slice){ 10, data }, arr);
+}
+
+static void test_initialize_from_buffer_with_more_data(void)
+{
+  TEST_IGNORE();
+  data_t data[20] = {
+    0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+  };
+  Array *arr = arrayFromCArray(20, data);
+  assert_clean_allocation(arr, 20, 20);
+  TEST_ASSERT_EQUAL_ARRAY((Slice){ 20, data }, arr);
+}
+
 int main(void)
 {
    UnityBegin("test/test_array.c");
@@ -56,6 +98,9 @@ int main(void)
    RUN_TEST(test_allocate_empty_array);
    RUN_TEST(test_allocate_array_with_capacity_too_small);
    RUN_TEST(test_allocate_array_with_capacity_larger);
+   RUN_TEST(test_initialize_from_buffer_empty);
+   RUN_TEST(test_initialize_from_buffer_with_data);
+   RUN_TEST(test_initialize_from_buffer_with_more_data);
 
    return UnityEnd();
 }
